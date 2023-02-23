b485171d20933efb68aa9cde2f769d190243070a
Vyacheslav Karpikov <vyacheslav.karpikov@tele2.kz>
2023-02-23 11:50:40 +0600

    2/2 Done

--- Challenge5/Program.cs
+++ Challenge5/Program.cs
@@ -1,24 +1,50 @@
 ﻿using System;
 
-namespace Challenge5
-{
-    class Program
-    {
-        static void Main(string[] args)
+namespace Challenge5 {
+    class Program {
+        static string[] SplitText()
         {
-            static string[] SplitText() {
-                string userText = Console.ReadLine();
-                string[] iResult;
-                iResult = userText.Split(" ");
-                return iResult;
+            string userText = Console.ReadLine();
+            string[] iResult;
+            iResult = userText.Split(" ");
+            return iResult;
+        }
+        static string[] SplitText(string text)
+        {
+            string[] iResult;
+            iResult = text.Split(" ");
+            return iResult;
+        }
+        static void PrintWords(string[] result) {
+            foreach (string word in result)
+            {
+                Console.WriteLine(word);
             }
-            static void PrintWords (string [] result) {
-                foreach (string word in result)
-                {
-                    Console.WriteLine(word);
-                }
+        }
+        static void PrintReversePhrase(string [] words) {
+            string resultReversePrase = "";
+            for (int i = words.Length-1; i >= 0; i--) {
+                resultReversePrase += words[i] + " ";
             }
+            Console.WriteLine(resultReversePrase);
+        }
+        static void ReverseWords(string inputPhrase)
+        {
+            string[] phraseWords = SplitText(inputPhrase);
+            PrintReversePhrase(phraseWords);
+        }
+        static void Main(string[] args) {
+            #region первое задание
+            Console.WriteLine("Первое задание, разделение строки на слова. Введите предложение: ");
             PrintWords(SplitText());
+            Console.ReadKey();
+            #endregion
+            Console.Clear();
+            #region второе задание
+            Console.WriteLine("Второе задание, перестановка слов в предложении. Введите предложение: ");
+            ReverseWords(Console.ReadLine());
+            Console.ReadKey();
+            #endregion
         }
     }
 }
