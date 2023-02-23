11a88cd6cf778e51d8ac3348137c7fa8930247d6
Vyacheslav Karpikov <vyacheslav.karpikov@tele2.kz>
2023-02-23 10:00:47 +0600

    Добавьте файлы проекта.

---
+++ Challenge5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Challenge5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static string[] SplitText() {
+                string userText = Console.ReadLine();
+                string[] iResult;
+                iResult = userText.Split(" ");
+                return iResult;
+            }
+            static void PrintWords (string [] result) {
+                foreach (string word in result)
+                {
+                    Console.WriteLine(word);
+                }
+            }
+            PrintWords(SplitText());
+        }
+    }
+}
